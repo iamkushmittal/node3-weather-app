fbd4398b4753eac90a560b4f8c90c5d5be838dd1
Kush Mittal <mittalkush1999@gmail.com>
2022-10-20 14:34:28 +0530

    Add high and low to Forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,8 +11,8 @@ forecast = (longitude,latitude,callback) =>{
             callback('Unable to find location. Try another Search.',undefined)
         } else{
             callback(undefined,
-                body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of Rain.' +
-                '\nHighest Temperature of the day is : ' + body.daily.data[0].temperatureHigh + ' degrees.' +'\n    Lowest Temperature of the day is : ' + body.daily.data[0].temperatureLow + ' degrees.'
+                body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees out.'+'The high today is ' + body.daily.data[0].temperatureHigh +' with a low of ' + body.daily.data[0].temperatureLow+'. There is a '+body.currently.precipProbability+'% chance of Rain.'
+                 
             )
         }
 
